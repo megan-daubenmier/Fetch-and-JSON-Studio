d4145403d7d13945d5a396ae7fecbe8d3a4bf4a9
Megan Daubenmier <megan.daubenmier@gmail.com>
2021-06-09 22:08:33 -0500

    completed second bonus mission

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ window.addEventListener("load", function() {
                     <h3>${json[i]["firstName"]} ${json[i]["lastName"]}</h3>
                     <ul>
                         <li>Hours in space: ${json[i]["hoursInSpace"]}</li>
-                        <li>Active: ${json[i]["active"]}</li>
+                        <li ${json[i].active ? 'id = green' : 'id = black'}>Active: ${json[i]["active"]}</li>
                         <li>Skills: ${json[i]["skills"].join(", ")}</li>
                     </ul>
                 </div>
